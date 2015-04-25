a489811da4d7c21056d0665c80774df09bd88b59
Schactman <markschactman@gmail.com>
2015-04-25 15:29:10 -0400

    Fixed a typo in the path name

--- run_analysis.R
+++ run_analysis.R
@@ -14,7 +14,7 @@ if (!file.exists("data")) {
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = ".//data//UCI HAR Dataset.zip")
 
-unzip(zipfile=".//data/UCI HAR Dataset.zip", exdir=".//dat")
+unzip(zipfile=".//data//UCI HAR Dataset.zip", exdir=".//dat")
 
 dateDownloaded <- date()
 dateDownloaded
